1991f570a5b85054dbeb6493c5b1998411a9d204
ezenwokeuchechukwu <ezenwokeuchechukwu@gmail.com>
2025-05-10 00:35:37 +0100

    completed

--- controllers/users.js
+++ controllers/users.js
@@ -4,12 +4,12 @@ const ObjectId = require('mongodb').ObjectId;
 const getAll = async (req, res) => {
   try {
     const result = await mongodb.getDatabase().db().collection('users').find();
-    result.toArray().then((users) => {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(users);
-    });
+    const users = await result.toArray();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(users);
   } catch (err) {
-    res.status(500).json({ message: 'Failed to retrieve users', error: err });
+    console.error("Error retrieving users:", err);
+    res.status(500).json({ message: 'Failed to retrieve users', error: err.message });
   }
 };
 
@@ -17,16 +17,16 @@ const getSingle = async (req, res) => {
   try {
     const userId = new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().db().collection('users').find({ _id: userId });
-    result.toArray().then((users) => {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(users[0]);
-    });
+    const users = await result.toArray();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(users[0]);
   } catch (err) {
-    res.status(500).json({ message: 'Failed to retrieve user', error: err });
+    console.error("Error retrieving user:", err);
+    res.status(500).json({ message: 'Failed to retrieve user', error: err.message });
   }
 };
 
 module.exports = {
   getAll,
-  getSingle,
+  getSingle
 };
